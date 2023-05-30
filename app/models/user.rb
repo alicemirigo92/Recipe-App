2fc4440bf4456b7fba6c5a7c7ee17e846b2f7717
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-05-30 21:09:39 +0100

    Add devise for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :foods
   has_many :recipes
   validates :name, presence: true
 end
 
-# has_many :foods, foreign_key: :user_id, dependent: :destroy
-# has_many :recipe, foreign_key: :user_id, dependent: :destroy
