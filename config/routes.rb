e69fa01c60f9112d01bf061b37e8df61ef69cc2b
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-06-03 06:55:50 +0100

    modify rroutes to avoid wasting resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
-  
-  resources :recipes do
-    resources :recipe_foods
+
+  resources :recipes, except: [:update, :edit] do
+    resources :recipe_foods, except: [:index]
   end
+  
   resources :foods
   resources :users
-  resources :shopping_list
-  resources :public_recipes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :shopping_list, only: [:index]
+  resources :public_recipes, only: [:index]
 end
+
